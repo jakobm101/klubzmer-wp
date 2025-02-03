a91f08d6f6b7b88f017754ce2962bc16b7bf1ca1
gestalt.root <gestalt@posteo.de>
2025-02-04 00:26:07 +0100

    rose pine

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <main>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article>
-                <h2><?php the_title(); ?></h2>
+                <h2 style="color: var(--gold);"><?php the_title(); ?></h2>
                 <?php the_post_thumbnail('medium'); ?>
                 <div><?php the_content(); ?></div>
             </article>
